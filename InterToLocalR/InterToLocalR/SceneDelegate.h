a305fbe9accafd47ac47ed70981cbfab38fa1c5e
chuanzhang <761406587@qq.com>
2021-01-27 11:27:25 +0800

    Add Project

---
+++ InterToLocalR/InterToLocalR/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  InterToLocalR
+//
+//  Created by chuanzhang on 2021/1/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ InterToLocalR/InterToLocalR/ViewController.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  InterToLocalR
+//
+//  Created by chuanzhang on 2021/1/27.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
